ce3fdb67013227be15b8eb481df48e70f0343380
Stephan Classen <st.classen@gmx.ch>
2017-08-26 09:46:52 +0200

    calling player

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,9 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 import java.util.Map;
 
@@ -9,7 +12,13 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        final JsonObject o = request.getAsJsonObject();
+        final JsonArray players = o.getAsJsonArray("players");
+        final int in_action = o.getAsJsonPrimitive("in_action").getAsInt();
+        final int current_buy_in = o.getAsJsonPrimitive("current_buy_in").getAsInt();
+        final int bet = players.get(in_action).getAsJsonObject().getAsJsonPrimitive("bet").getAsInt();
+
+        return current_buy_in - bet;
     }
 
     public static void showdown(JsonElement game) {
