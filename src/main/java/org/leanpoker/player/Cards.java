a13d25d459e7459707840762639a388743e75ddb
Stephan Classen <st.classen@gmx.ch>
2017-08-26 10:23:49 +0200

    log

--- src/main/java/org/leanpoker/player/Cards.java
+++ src/main/java/org/leanpoker/player/Cards.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  * ...
@@ -12,6 +13,6 @@ public class Cards {
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this);
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 }
--- src/main/java/org/leanpoker/player/GameState.java
+++ src/main/java/org/leanpoker/player/GameState.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  * ...
@@ -24,6 +25,6 @@ public class GameState {
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this);
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 }
